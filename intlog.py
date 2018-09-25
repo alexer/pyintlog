8b41f23dfc4e5e5fe7e0ba8d83134c12b2977409
Aleksi Torhamo <aleksi@torhamo.net>
2018-09-26 00:37:04 +0300

    Added fast_*_log() - *_log() no longer requires manual range extension
    The default *_log() functions now automatically extend the range when
    required. Added fast_*_log() which behaves like *_log() used to, minus
    the error checking.

--- intlog.py
+++ intlog.py
@@ -19,15 +19,17 @@ many environments, log(125, 5) returns 3.0000000000000004 instead of 3, which me
 implemented as ceil(log(value + 1, base)) - would return 4 instead of the correct 3.
 
 The functions in this module use a lookup table to speed up the calculation, so their time complexity is O(1)
-instead of the usual O(log N). They require a call to extend_intlog_range_to_*() - which is O(log N) - before the
-first calculation, to initialize the aforementioned lookup table.
+instead of the usual O(log N). The fast_*_log() functions are strictly O(1), but require an explicit call to
+extend_fast_intlog_range_to_*() - which is O(log N) - before the first calculation, to initialize the
+aforementioned lookup table. The *_log() functions extend the range automatically as required, so their complexity
+is amortized O(1).
 
 The space requirement of the lookup table is O(log N) per each used base; Logarithms of values up to 2**64 in any
 base can be calculated with a lookup table of around 64 elements. The LUT method is 2-10x faster than the usual
 method on typical inputs, although we are talking about only microseconds here. (The usual method can be ~30% - or
 a fraction of a microsecond - faster when the given value is smaller than the base itself)
 
-Note: You must call extend_intlog_range_to_*() before the first call to any of the *_log() functions.
+Note: You must call extend_fast_intlog_range_to_*() before the first call to any of the fast_*_log() functions.
 """
 from __future__ import unicode_literals, print_function
 import operator
@@ -64,7 +66,7 @@ def _init_next_power(limits, power, value):
 		limits.append((power, value))
 	return bitlen
 
-def extend_intlog_range_to_power(max_power, base):
+def extend_fast_intlog_range_to_power(max_power, base):
 	"""Initialize or extend the range supported by the integer logarithm functions so that all integers up to base ** max_power are supported for the given base"""
 	assert isinstance(base, int)
 	limits = _lut.setdefault(base, [None, (0, 1)])
@@ -72,8 +74,9 @@ def extend_intlog_range_to_power(max_power, base):
 	for power in range(min_power + 1, max_power + 1):
 		value *= base
 		_init_next_power(limits, power, value)
+	return power, value
 
-def extend_intlog_range_to_bitlen(max_bitlen, base):
+def extend_fast_intlog_range_to_bitlen(max_bitlen, base):
 	"""Initialize or extend the range supported by the integer logarithm functions so that all integers up to the given bit length are supported for the given base"""
 	assert isinstance(base, int)
 	limits = _lut.setdefault(base, [None, (0, 1)])
@@ -83,6 +86,7 @@ def extend_intlog_range_to_bitlen(max_bitlen, base):
 		value *= base
 		power += 1
 		bitlen = _init_next_power(limits, power, value)
+	return power, value
 
 
 def gt_log(value, base):
@@ -90,12 +94,13 @@ def gt_log(value, base):
 
 	That is, the returned power satisfies: base ** (power - 1) <= value < base ** power
 	This is also the exact integer equivalent of: ceil(log(value + 1, base))
-
-	Note: You must call extend_intlog_range_to_*() before the first call to this function.
 	"""
 	if value <= 0:
 		raise ValueError('Logarithm is only defined for numbers greater than zero (the power approaches negative infinity as the value approaches zero)')
-	power, limit = _lut[base][value.bit_length()]
+	try:
+		power, limit = _lut[base][value.bit_length()]
+	except (KeyError, IndexError):
+		power, limit = extend_fast_intlog_range_to_bitlen(value.bit_length(), base)
 	return power + (value >= limit)
 
 def ge_log(value, base):
@@ -103,12 +108,13 @@ def ge_log(value, base):
 
 	That is, the returned power satisfies: base ** (power - 1) < value <= base ** power
 	This is also the exact integer equivalent of: ceil(log(value, base))
-
-	Note: You must call extend_intlog_range_to_*() before the first call to this function.
 	"""
 	if value <= 0:
 		raise ValueError('Logarithm is only defined for numbers greater than zero (the power approaches negative infinity as the value approaches zero)')
-	power, limit = _lut[base][value.bit_length()]
+	try:
+		power, limit = _lut[base][value.bit_length()]
+	except (KeyError, IndexError):
+		power, limit = extend_fast_intlog_range_to_bitlen(value.bit_length(), base)
 	return power + (value > limit)
 
 def lt_log(value, base):
@@ -116,12 +122,13 @@ def lt_log(value, base):
 
 	That is, the returned power satisfies: base ** power < value <= base ** (power + 1)
 	This is also the exact integer equivalent of: floor(log(value - 1, base))
-
-	Note: You must call extend_intlog_range_to_*() before the first call to this function.
 	"""
 	if value <= 0:
 		raise ValueError('Logarithm is only defined for numbers greater than zero (the power approaches negative infinity as the value approaches zero)')
-	power, limit = _lut[base][value.bit_length()]
+	try:
+		power, limit = _lut[base][value.bit_length()]
+	except (KeyError, IndexError):
+		power, limit = extend_fast_intlog_range_to_bitlen(value.bit_length(), base)
 	return power - (value <= limit)
 
 def le_log(value, base):
@@ -129,12 +136,13 @@ def le_log(value, base):
 
 	That is, the returned power satisfies: base ** power <= value < base ** (power + 1)
 	This is also the exact integer equivalent of: floor(log(value, base))
-
-	Note: You must call extend_intlog_range_to_*() before the first call to this function.
 	"""
 	if value <= 0:
 		raise ValueError('Logarithm is only defined for numbers greater than zero (the power approaches negative infinity as the value approaches zero)')
-	power, limit = _lut[base][value.bit_length()]
+	try:
+		power, limit = _lut[base][value.bit_length()]
+	except (KeyError, IndexError):
+		power, limit = extend_fast_intlog_range_to_bitlen(value.bit_length(), base)
 	return power - (value < limit)
 
 def int_log(value, base, rounding):
@@ -142,17 +150,57 @@ def int_log(value, base, rounding):
 
 	The rounding argument should be one of the comparison operators >, ≥, <, or ≤; It determines how
 	the returned integer is chosen in relation to the real-valued return value of log(value, base).
-
-	Note: You must call extend_intlog_range_to_*() before the first call to this function.
 	"""
 	if value <= 0:
 		raise ValueError('Logarithm is only defined for numbers greater than zero (the power approaches negative infinity as the value approaches zero)')
+	add_op, cmp_op = _ROUNDING[rounding]
+	try:
+		power, limit = _lut[base][value.bit_length()]
+	except (KeyError, IndexError):
+		power, limit = extend_fast_intlog_range_to_bitlen(value.bit_length(), base)
+	return add_op(power, cmp_op(value, limit))
+
+
+def fast_gt_log(value, base):
+	power, limit = _lut[base][value.bit_length()]
+	return power + (value >= limit)
+
+def fast_ge_log(value, base):
+	power, limit = _lut[base][value.bit_length()]
+	return power + (value > limit)
+
+def fast_lt_log(value, base):
+	power, limit = _lut[base][value.bit_length()]
+	return power - (value <= limit)
+
+def fast_le_log(value, base):
+	power, limit = _lut[base][value.bit_length()]
+	return power - (value < limit)
+
+def fast_int_log(value, base, rounding):
 	add_op, cmp_op = _ROUNDING[rounding]
 	power, limit = _lut[base][value.bit_length()]
 	return add_op(power, cmp_op(value, limit))
 
 
-def _test_intlog_funcs(funcs, max_value, bases, precision=14, verbose=False):
+def _copy_docstrings(doc_suffix):
+	"""Copy docstrings from the default functions to the fast variant, with the given extra documentation"""
+	globs = globals()
+	for variant in 'gt ge lt le int'.split():
+		src_name = '_'.join((variant, 'log'))
+		dst_name = '_'.join(('fast', variant, 'log'))
+		doc = globs[src_name].__doc__
+		if doc_suffix:
+			delim = '\n' + doc.rsplit('\n')[-1]
+			doc = delim.join((doc, doc_suffix, ''))
+		globs[dst_name].__doc__ = doc
+
+_copy_docstrings(
+	'Note: You must call extend_fast_intlog_range_to_*() before the first call to this function.',
+)
+
+
+def _test_intlog_funcs(funcs, max_value, bases, precision=14, extend_range=False, verbose=False):
 	"""Test the given integer logarithm functions up to max_value for all the given bases
 
 	A note on the precision argument:
@@ -169,7 +217,8 @@ def _test_intlog_funcs(funcs, max_value, bases, precision=14, verbose=False):
 	for base in bases:
 		if verbose:
 			print('Testing base', base)
-		extend_intlog_range_to_bitlen(max_value.bit_length(), base)
+		if extend_range:
+			extend_fast_intlog_range_to_bitlen(max_value.bit_length(), base)
 		for value in range(1, max_value + 1):
 			power = gt_func(value, base)
 			assert base ** (power - 1) <= value < base ** power
@@ -191,14 +240,16 @@ def _test_all_intlog_funcs(max_value, bases, precision=14, verbose=False):
 	ops = 'gt ge lt le'.split()
 	test_funcs = partial(_test_intlog_funcs, max_value=max_value, bases=bases, precision=precision, verbose=verbose)
 
-	funcs = [globs[op + '_log'] for op in ops]
-	if verbose:
-		print('Testing functions:', ' '.join(func.__name__ for func in funcs))
-	test_funcs(funcs)
+	for speed in ' fast_'.split(' '):
+		funcs = [globs[speed + op + '_log'] for op in ops]
+		if verbose:
+			print('Testing functions:', ' '.join(func.__name__ for func in funcs))
+		test_funcs(funcs)
 
-	if verbose:
-		print('Testing function: int_log')
-	test_funcs([partial(int_log, rounding=op) for op in ops])
+		func = globs[speed + 'int_log']
+		if verbose:
+			print('Testing function:', func.__name__)
+		test_funcs([partial(func, rounding=op) for op in ops])
 
 if __name__ == '__main__':
 	_test_all_intlog_funcs(10**6, range(2, 11), verbose=True)
